315df2594c61378e3f583aeeefcd65ef250d62ef
Pedro Amaral <pedropaulo@life.com.br>
2020-01-05 21:34:18 -0300

    primeiro commit

---
+++ backend/app/Models/Team.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Team extends Model {
+  static boot() {
+    super.boot()
+
+    this.addTrait('@provider:Lucid/Slugify', {
+      fields: {
+        slug: 'name'
+      },
+      strategy: 'dbIncrement',
+      disableUpdates: false
+    })
+  }
+
+
+  users(){
+    //Pivot permite que toda vez que ocorrer esse relacionamento é possivel manupular o userTeam
+    return this.belongsToMany('App/Models/User').pivotModel(
+      "App/Models/UserTeam"
+    );
+  }
+
+  projects(){
+    return this.hasMany('App/Models/Project')
+  }
+}
+
+module.exports = Team
